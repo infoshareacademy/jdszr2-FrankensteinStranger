eb3711cd0e725beee37c955954f776d0d851c5e7
Karolina Warda <karolimera@gmail.com>
2020-11-09 18:03:13 +0100

    update pliku analiza_customer

--- Projekt 1 - SQL/analiza_customer_KW.sql
+++ Projekt 1 - SQL/analiza_customer_KW.sql
@@ -19,12 +19,7 @@ SELECT count(DISTINCT(customer_id)) AS cnt_customer,
 	count (DISTINCT(CASE when return_date IS NULL THEN customer_id end)) AS niezwrocone
 FROM t1
 
--- ilość klientów, w których koszyku:
--- 1. ponad 50% filmów jest zwróconych z opóźnieniem
--- 2. między 50% a 10% filmów jest zwróconych z opóźnieniem
--- 3. mniej niż 10% filmów jest zwróconych z opóźnieniem
--- 4. ilość klientów, którzy choć raz nie zwrócili filmu
--- 5. ilośc klientów, dla którzy nie zwracają 5% lub więcej swoich zamówień (ale mniej niż 10%)
+-- procent filmów zwracanych po terminie lub niezwracanych wcale przez klientów
 
 WITH t1 AS
 (
@@ -45,27 +40,19 @@ ORDER BY f.film_id
 ), 
 t2 AS 
 (
-SELECT customer_id,
-	count(DISTINCT(rental_id)) AS rent_cnt,
-	count(CASE WHEN status_zwrotu = 'opozniony' THEN rental_id END) AS ilosc_opoznionych,
-	count(CASE WHEN status_zwrotu = 'niezwrocony' THEN rental_id END ) AS ilosc_niezwroconych
+SELECT customer_id, 
+	count(rental_id) AS cnt_rent,
+	count(CASE WHEN status_zwrotu = 'opozniony' THEN rental_id END) AS cnt_opoznione,
+	count(CASE WHEN status_zwrotu = 'niezwrocony' THEN rental_id END) AS cnt_niezwrocony
 FROM t1
 GROUP BY customer_id
-ORDER BY 3 desc
-),
-t3 AS 
-( 
-SELECT customer_id,
-	(ilosc_opoznionych::numeric/rent_cnt) AS wsp_opoznionych,
-	(ilosc_niezwroconych::numeric/rent_cnt) AS wsp_niezwroconych
-FROM t2
-ORDER BY 2 DESC
 )
 
-SELECT count(DISTINCT(customer_id)) AS cnt_customer,
-	count(CASE WHEN wsp_opoznionych > 0.5 THEN customer_id END) AS ponad_50proc_opoznionych,
-	count(CASE WHEN wsp_opoznionych < 0.5 AND wsp_opoznionych > 0.1 THEN customer_id END) AS miedzy_50_i_10_proc,
-	count(CASE WHEN wsp_opoznionych < 0.1 THEN customer_id END) AS mniej_niz_10_proc,
-	count(CASE WHEN wsp_niezwroconych > 0 THEN customer_id END) AS niezwrone,
-	count(CASE WHEN wsp_niezwroconych >= 0.05 THEN customer_id END) AS wiecej_niz_5_proc_niezwroconych
-FROM t3
+SELECT 
+	round(cnt_opoznione/cnt_rent::NUMERIC,2) AS proc_opoznione,
+	--round(cnt_niezwrocony/cnt_rent::NUMERIC,2) AS proc_niezwrocony,
+	count(customer_id) AS ilosc_klientow 
+FROM t2
+GROUP BY round(cnt_opoznione/cnt_rent::NUMERIC,2)
+--round(cnt_niezwrocony/cnt_rent::NUMERIC,2)
+ORDER BY 1 desc
