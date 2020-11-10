36db37ef459cab801a93cfd7865376ae97d1563b
Anna Woźniak <pannaannamariawozniak@gmail.com>
2020-11-10 17:21:28 +0100

    Aktualizacja skryptów

--- Projekt 1 - SQL/Analizy_kategorie_AW.sql
+++ Projekt 1 - SQL/Analizy_kategorie_AW.sql
@@ -10,6 +10,15 @@ create view customer_city_country as
 		join city c2 on a.city_id = c2.city_id
 		join country c3 on c2.country_id = c3.country_id
 
+-- adresy sklepów
+
+select
+	*
+from store s
+	join address a on s.address_id = a.address_id
+	join city c on a.city_id = c.city_id
+	join country c2 on c.country_id = c2.country_id 
+
 -- liczba filmów w danej kategorii
 select 
 	distinct c.name, 
@@ -20,7 +29,47 @@ from film f
 		group by c.name
 		order by 2 DESC
 
--- liczba filmów w danej kategorii z podziałem na wypozyczalnię 
+-- liczba kopii w danej kategorii
+
+select  
+	c.name, 
+	count(*)
+from film f
+	join film_category fc on f.film_id = fc.film_id 
+	join category c on fc.category_id = c.category_id 
+	join inventory i on f.film_id = i.film_id 
+	join store s on i.store_id = s.store_id 
+		group by 1
+		order by 1,2 desc 	
+
+-- liczba tytułów i kopii w podziale na kategorie
+
+with aaa as 
+(select  
+	c.name as kategoria, 
+	count(*) as l_kopii
+from film f
+	join film_category fc on f.film_id = fc.film_id 
+	join category c on fc.category_id = c.category_id 
+	join inventory i on f.film_id = i.film_id 
+	join store s on i.store_id = s.store_id 
+		group by 1
+		order by 1,2 desc),
+bbb as		
+(select 
+	distinct c.name kategoria, 
+	count(*) as l_filmow
+from film f
+	join film_category fc on f.film_id = fc.film_id 
+	join category c on fc.category_id = c.category_id 
+		group by c.name
+		order by 2 desc),
+ccc as 
+(select a.kategoria, l_kopii, l_filmow from aaa a  join bbb b on a.kategoria = b.kategoria)
+select * from ccc
+		
+
+-- liczba kopii w danej kategorii z podziałem na wypozyczalnię 
 
 select 
 	s.store_id, 
@@ -72,6 +121,8 @@ from rental r
 	left join payment p on r.rental_id = p.rental_id 
 		group by 1
 		order by 2 desc
+
+-- przychody + liczba wypozyczen z podzialem na kategorie		
 
 -- cennik filmów + lista wypozyczen
 
@@ -228,3 +279,40 @@ select
 from ccc 
 	group by 1,2,3,4
 	order by 3 desc 
+
+-- rating -> przychód, liczba kopii, liczba tytułów, liczba wypozyczen
+
+with aaa as
+(select 
+	f.rating as rating, 
+	sum(p.amount) as przychód,
+	count(r.rental_id) as l_wypoz
+from rental r 
+	join inventory i on r.inventory_id = i.inventory_id 
+	join film f on i.film_id = f.film_id 
+	join film_category fc  on f.film_id = fc.film_id
+	join category c on fc.category_id = c.category_id 
+	left join payment p on r.rental_id = p.rental_id 
+		group by 1),
+bbb as		
+(select 
+	f2.rating as rating, 
+	count(i2.inventory_id) as liczba_kopii 
+from film f2 
+	join inventory i2 on f2.film_id = i2.film_id
+		group by 1),	
+ccc as 		
+(select 
+	f2.rating as rating, 
+	count(f2.film_id) as liczba_tyt
+from film f2 
+		group by 1)		
+select 
+	aaa.rating, 
+	aaa.przychód, 
+	aaa.l_wypoz, 
+	bbb.liczba_kopii, 
+	ccc.liczba_tyt 
+from ccc 
+	join bbb on ccc.rating = bbb.rating 
+	join aaa on bbb.rating = aaa.rating	
